1030d879db4693d106adc0fe663bd1816c8963ec
Quoc khang Luong <khanghuu97@gmail.com>
2024-04-19 22:13:12 +0700

    Add project files.

---
+++ src/Services/Catalog/EShop.Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,5 @@
+﻿namespace EShop.Catalog.API.Products.CreateProduct;
+
+public class CreateProductEndpoint
+{
+}
---
+++ src/Services/Catalog/EShop.Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,31 @@
+﻿using BuildingBlocks.CQRS;
+using EShop.Catalog.API.Models;
+
+namespace EShop.Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name,
+    List<string> Category,
+    string Description,
+    string ImageFile,
+    decimal Price) : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        Product product = new()
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        return new CreateProductResult(new Guid());
+    }
+}
